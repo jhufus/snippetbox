492c20b8ca91788e7c0910c996655879e67cbb6c
fts63 <fts63@mail.ru>
2024-04-05 10:28:58 +0700

    added flag with config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,52 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
+
+	flag.Parse()
 
 	mux := http.NewServeMux() // инициализация нового рутера
+
+	//fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static/")})
+	//mux.Handle("/static", http.NotFoundHandler())
+	//mux.Handle("/static/", http.StripPrefix("./static", fileServer))
 	mux.HandleFunc("/", home) // "home" регистрируется как обработчик для URL-шаблона "/"
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	fileServer := http.FileServer(http.Dir("C:\\Users\\XIaomi\\go\\src\\github.com\\jhufus\\snippetbox\\ui\\static"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting web-server http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux) // запуск нового веб-сервера (TCP-адрес сети для прослушивания, роутер)
+	log.Printf("Starting web-server %s", *addr)
+	err := http.ListenAndServe(*addr, mux) // запуск нового веб-сервера (TCP-адрес сети для прослушивания, роутер)
 	log.Fatal(err)
 }
+
+//type neuteredFileSystem struct {
+//	fs http.FileSystem
+//}
+//
+//func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+//	f, err := nfs.fs.Open(path)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	s, err := f.Stat()
+//	if s.IsDir() {
+//		index := filepath.Join(path, "index.html")
+//		if _, err := nfs.fs.Open(index); err != nil {
+//			closeErr := f.Close()
+//			if closeErr != nil {
+//				return nil, closeErr
+//			}
+//		}
+//	}
+//	return f, nil
+//}
